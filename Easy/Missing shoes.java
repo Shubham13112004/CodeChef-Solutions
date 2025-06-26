40e1c146faf27b40706046b1267f45b0421b48f7
Shubham13112004 <158588207+Shubham13112004@users.noreply.github.com>
2025-06-26 15:50:22 +0530

    Create Missing shoes.java

---
+++ Easy/Missing shoes.java
@@ -0,0 +1,25 @@
+//Missing Shoes
+//link:-https://www.codechef.com/problems/SHOM
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int L =sc.nextInt();
+	    int R =sc.nextInt();
+	    
+	    
+	    if(L>=R){
+	    System.out.println(L-R);
+	    }
+	    else{
+	    System.out.println(R-L);
+	    }
+	    
+	}
+}
