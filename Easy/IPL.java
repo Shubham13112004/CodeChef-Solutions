33c257cf4c0c7c88db91a1fc1a787b80ef581f3c
Shubham13112004 <158588207+Shubham13112004@users.noreply.github.com>
2025-06-29 15:10:47 +0530

    Create IPL.java

---
+++ Easy/IPL.java
@@ -0,0 +1,24 @@
+//IPL 
+//https://www.codechef.com/problems/INPL
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int x= sc.nextInt();
+		
+		if(x>=7){
+		    System.out.println("THALA");
+		}
+		else{
+		    System.out.println("BOOM");
+		}
+
+	}
+}
