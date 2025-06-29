ed7cbfacc861f3c318c97333cf4841323a0e4452
Shubham13112004 <158588207+Shubham13112004@users.noreply.github.com>
2025-06-30 02:53:14 +0530

    Create AI is coming.java

---
+++ Easy/AI is coming.java
@@ -0,0 +1,26 @@
+//AI is Coming
+//http://codechef.com/problems/AICOM
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		if(x<=60){
+		    
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		    
+		}
+
+	}
