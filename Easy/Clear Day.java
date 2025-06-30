1c261f6cd28ecbc52aa08301b8525827dc8a59d1
Shubham13112004 <158588207+Shubham13112004@users.noreply.github.com>
2025-06-30 16:06:04 +0530

    Create Clear Day.java

---
+++ Easy/Clear Day.java
@@ -0,0 +1,22 @@
+//Clear Day
+//https://www.codechef.com/problems/CLEARDAY
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		int ans = 7-x-y;
+		
+		System.out.println(ans);
+
+	}
+}
