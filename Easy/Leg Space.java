ca4e311772111bca9e437a870b814d7c931521fa
Shubham13112004 <158588207+Shubham13112004@users.noreply.github.com>
2025-06-30 16:14:03 +0530

    Create Leg Space.java

---
+++ Easy/Leg Space.java
@@ -0,0 +1,27 @@
+//Leg Space
+//https://www.codechef.com/problems/LEGSP
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		if(n<m){
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("NO");
+		}
+		    
+		
+
+	}
+}
