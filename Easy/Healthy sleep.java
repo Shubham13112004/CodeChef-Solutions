2d892a5e27236f01d9085e9b781dfff8565e31ec
Shubham13112004 <158588207+Shubham13112004@users.noreply.github.com>
2025-07-01 02:37:20 +0530

    Create Healthy sleep.java

---
+++ Easy/Healthy sleep.java
@@ -0,0 +1,26 @@
+//Healthy sleep
+//https://www.codechef.com/problems/HEALSE
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int x=sc.nextInt();
+		
+		if(x<8){
+		    System.out.println("LESS");
+		}else if(x==8){
+		    System.out.println("PERFECT");
+		}
+		else{
+		    System.out.println("MORE");
+		}
+
+	}
+}
