25a6d6afca21307fc365017c3ed8beb66b1b868c
Shubham13112004 <158588207+Shubham13112004@users.noreply.github.com>
2025-06-30 18:58:19 +0530

    Create IOI 2024.java

---
+++ Easy/IOI 2024.java
@@ -0,0 +1,24 @@
+//IOI 2024
+//https://www.codechef.com/problems/IOI2024
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		
+		if(x>=1 && x<=8){
+		    System.out.println("YES");
+		}
+		else{
+		    System.out.println("NO");
+		}
+
+	}
+}
