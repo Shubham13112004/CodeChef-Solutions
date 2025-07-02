06e8a3daf2c741b341a29648a28095a772ba124e
Shubham13112004 <158588207+Shubham13112004@users.noreply.github.com>
2025-07-02 07:53:52 +0530

    Create Reach home.java

---
+++ Easy/Reach home.java
@@ -0,0 +1,27 @@
+//https://www.codechef.com/problems/REACH_HOME
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t=sc.nextInt();
+		while(t-->0){
+		    int x = sc.nextInt();
+		    int y = sc.nextInt();
+		    int result = 5*x;
+		    
+		    if(result>=y){
+		        System.out.println("YES");
+		    }
+		    else{
+		        System.out.println("NO");
+		    }
+		}
+
+	}
+}
